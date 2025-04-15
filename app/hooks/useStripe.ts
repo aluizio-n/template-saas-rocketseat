bc8a0abc0ee2bf3ce4a3a6bd63fe9d1fa54713ab
Aluizio Neto <aluizio.sln@gmail.com>
2025-04-15 20:52:03 -0300

    feat: add stripe and stripe hook

---
+++ app/hooks/useStripe.ts
@@ -0,0 +1,88 @@
+import { useEffect, useState } from "react";
+import { loadStripe, Stripe } from "@stripe/stripe-js"
+
+export function useStripe() {
+    const [stripe, setStripe] = useState<Stripe | null>(null);
+
+    useEffect(()=>{
+        async function loadStripeAsync() {
+            const stripeInstance = await loadStripe(
+                process.env.NEXT_PUBLIC_STRIPE_PUB_KEY!
+            );
+            setStripe(stripeInstance);
+        }
+
+        loadStripeAsync();
+    },[]);
+
+    async function createPaymentStripeCheckout(checkoutData:unknown) {
+        if(!stripe) return
+
+        try {
+            const response = await fetch("/api/stripe/create-pay-checkout", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(checkoutData),
+            })
+
+            const data = await response.json()
+
+            await stripe.redirectToCheckout({
+                sessionId: data.id,
+            });
+
+        } catch (error) {
+            console.error("Error creating checkout session:", error);
+        }
+    }
+
+    async function createSubscriptionStripeCheckout(checkoutData: unknown){
+        if(!stripe) return;
+
+        try {
+            const response = await fetch("/api/stripe/create-subscription-checkout", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(checkoutData),
+            });
+
+            const data = await response.json();
+
+            await stripe.redirectToCheckout({
+                sessionId: data.id,
+            });
+
+        } catch (error) {
+            console.error("Error creating subscription checkout session:", error);
+            
+        }
+    }
+
+    async function handleCreateStripePortal(){
+        try {
+            const response = await fetch("/api/stripe/create-portal", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            })
+
+            const data = await response.json()
+
+            window.location.href = data.url
+
+        } catch (error) {
+            console.error("Error creating checkout session:", error);
+        }
+    }
+
+    return {
+        createPaymentStripeCheckout,
+        createSubscriptionStripeCheckout,
+        handleCreateStripePortal,
+    }
+}
